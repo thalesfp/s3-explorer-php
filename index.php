adafddabcc584b983e5c5bdfb28bb2d641a228a7
Thales Pereira <thalesfp@gmail.com>
2014-06-05 23:17:31 -0300

    Rename twig filters file and add config.ini

--- index.php
+++ index.php
@@ -2,27 +2,37 @@
 
 require 'vendor/autoload.php';
 require 'S3.class.php';
-require 'twig.filters.php';
+require 'twig_filters.php';
 
-$access_key = "";
-$secret_key = "";
+$ini_array = parse_ini_file("config.ini");
+
+if (array_key_exists("access_key", $ini_array) and array_key_exists("secret_key", $ini_array)) {
+	$access_key = $ini_array["access_key"];
+	$secret_key = $ini_array["secret_key"];
+} else {
+	die("Please set access_key and secret_key in config.ini file.");
+}
 
 $s3 = new S3($access_key, $secret_key);
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-$twig->addFilter($filter1);
-$twig->addFilter($filter2);
-$twig->addFilter($filter3);
+$twig->addFilter($filter_format_bytes);
+$twig->addFilter($filter_table_selected);
+$twig->addFilter($filter_format_datetime);
 
-$buckets = $s3->listBuckets();
+if (array_key_exists("bucket", $ini_array)) {
+	$buckets = NULL;
+	$selected_bucket = $ini_array["bucket"];
+} else {
+	$buckets = $s3->listBuckets();
 
-if(isset($_POST["bucket"]))
-	$selected_bucket = $_POST["bucket"];
-else
-	$selected_bucket = $buckets[0]["Name"];
+	if(isset($_POST["bucket"]))
+		$selected_bucket = $_POST["bucket"];
+	else
+		$selected_bucket = $buckets[0]["Name"];
+}
 
 $objects = $s3->listObjects($selected_bucket);
-
 echo $twig->render('index.html', array("buckets" => $buckets, "objects" => $objects));
--- twig_filters.php
+++ twig_filters.php
@@ -8,7 +8,7 @@
  *
  * @return string The bytes converted 
 */
-$filter1 = new Twig_SimpleFilter('formatBytes', function($bytes, $precision = 2) {
+$filter_format_bytes = new Twig_SimpleFilter('formatBytes', function($bytes, $precision = 2) {
 	$units = array('B', 'KB', 'MB', 'GB', 'TB'); 
 
     $bytes = max($bytes, 0); 
@@ -20,12 +20,12 @@ $filter1 = new Twig_SimpleFilter('formatBytes', function($bytes, $precision = 2)
     return round($bytes, $precision) . ' ' . $units[$pow]; 
 });
 
-$filter2 = new Twig_SimpleFilter('selected', function($bucket) {
+$filter_table_selected = new Twig_SimpleFilter('selected', function($bucket) {
 	if (isset($_POST["bucket"]) and $bucket == $_POST["bucket"])
 		return "selected";
 });
 
-$filter3 = new Twig_SimpleFilter('formatDateTime', function($date) {
+$filter_format_datetime = new Twig_SimpleFilter('formatDateTime', function($date) {
 	$date = new DateTime($date);	
 	return $date->format('Y-m-d H:i:s');
 });
